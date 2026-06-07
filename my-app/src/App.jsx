d073e46534f145f2e41986ea09bc81ce45628f65
Mourtalla Diop <mourtalladiop127@gmail.com>
2026-06-07 04:02:56 +0000

    Update React app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,46 @@
 
 import './App.css'
-function Header() {
+
+
+
+function Header(props) {
+  console.log(props)
   return (
     <header>
-      <h1>My App</h1>
+      <h1>{props.name}</h1>
+      <p> Copyright {props.year}</p>
     </header>
+  )
+}
+const items = [
+  "Drole de Dame",
+  "Sousou",
+  "Toto Et Ninetta"
+];
+const dishObject =  items.map((dish, i) => ({ 
+    id: i, 
+    title: dish 
+  }));
+  console.log(dishObject)
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyle: 'none' }}>
+          {dish.title}
+          </li>
+      ))}
+    </ul>
   )
 }
 function App() {
   return ( 
-<div>
-    <Header />
-    <main>
-      <h2>Welcome to my app!</h2>
-    </main>
- </div>
-  );
+    <div className="app-shell">
+      <Header name="D'or Et de Platine" year={new Date().getFullYear()} />
+      <Main dishes={dishObject} />
+    </div>
+  )
 }
 
 export default App
