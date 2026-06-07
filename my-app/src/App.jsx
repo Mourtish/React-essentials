66213e13a65f92ce861e2c874e07eb1140619f82
Mourtalla Diop <mourtalladiop127@gmail.com>
2026-06-07 02:29:48 +0000

    Update React app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+function Header() {
+  return (
+    <header>
+      <h1>My App</h1>
+    </header>
+  )
+}
+function App() {
+  return ( 
+<div>
+    <Header />
+    <main>
+      <h2>Welcome to my app!</h2>
+    </main>
+ </div>
+  );
+}
+
+export default App
